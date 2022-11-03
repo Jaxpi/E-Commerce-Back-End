ce6399af3a5a691d6b106564c63163f0bb1caf2c
Jax I <jaxpi85@gmail.com>
2022-11-03 15:37:08 -0400

    added column definitions to .js files

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,21 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      isNumeric: true,
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
